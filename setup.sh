e65e921c90c0ae41d6c8de1ab9b1e33faff5a413
Mathurian <34145134+Mathurian@users.noreply.github.com>
2025-10-23 10:47:06 -0500

    journal error 3

--- setup.sh
+++ setup.sh
@@ -4580,8 +4580,7 @@ create_backend_files() {
     print_status "Creating essential backend files..."
     
     # Create directories
-    mkdir -p "$APP_DIR/src/database" "$APP_DIR/src/controllers" "$APP_DIR/src/middleware" \
-        "$APP_DIR/src/routes" "$APP_DIR/src/socket" "$APP_DIR/src/utils"
+    mkdir -p "$APP_DIR/src/database" "$APP_DIR/src/controllers" "$APP_DIR/src/middleware" "$APP_DIR/src/routes" "$APP_DIR/src/socket" "$APP_DIR/src/utils"
     
     # Create modular middleware files
     create_middleware_files
